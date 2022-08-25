88c2137706f15c029920456496e4132afa7a3de4
Luiza Ribeiro <ribeiroluiza.jlle@gmail.com>
2022-08-24 23:39:38 -0300

    embaralhar

--- teste.py
+++ teste.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import random
+
+
+def Embaralhar(palavra):
+    novo = list(palavra)
+    random.shuffle(novo)
+    novo = "".join(novo)
+    print(novo.lower())
+
+    
+palavra = "maluca"
+Embaralhar(palavra)
+
+resposta = print(input("Digite a resposta: "))
+
+if resposta == Embaralhar(palavra):
+    print("Acertou!")
+elif resposta != Embaralhar(palavra):
+    print("Tente novamente!")
+
+
