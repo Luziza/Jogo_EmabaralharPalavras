48f503e2b8fe53fc198b71dcae34b3349ba75d48
Luiza Ribeiro <50594118+Luziza@users.noreply.github.com>
2022-08-29 11:44:48 -0300

    Create embaralhar_avancaco.py

---
+++ embaralhar_avancaco.py
@@ -0,0 +1,38 @@
+import random
+
+def Temas():
+    cidade = ["joinville", "araquari", "natal"]
+    cores = ["vermelho", "amarelo", "azul"]
+    paises = ["brasil", "japão", "chile"]
+
+    escolha = input("Escolha o tema: ")
+
+    if escolha == cidade:
+        
+
+def Embaralhar(palavra):
+    novo = list(palavra)
+    random.shuffle(novo)
+    novo = "".join(novo)
+    print(novo.lower())
+
+def Ajuda():
+    lista = ["Você consegue", "Tente novamente", "A maior prova de coragem é suportar as derrotas sem perder o ânimo.", "Acredite que você pode, assim você já está no meio do caminho."]
+    random.shuffle(lista)
+    return lista[0]
+
+    
+palavra = "atumalaca"
+Embaralhar(palavra)
+
+
+for i in range(5):
+    resposta =(input("Digite a resposta: "))
+    if resposta == palavra:
+        print("Acertou! Você utilizou " + str(i) + " tentivas")
+        break
+    elif resposta != palavra:
+        print(Ajuda())
+    if i == 4:
+        print("A resposta certa era " + palavra)
+
